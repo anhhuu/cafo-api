95369a4c75440b4bc41dcde59dd4eacc77c2228d
panhhuu@gmail.com <panhhuu@gmail.com>
2020-10-27 10:21:52 +0700

    add cors lib

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const route = require('./routes/index')
 const db = require('./config/db');
-
+const cors = require('cors')
 const app = express();
 const port = 3333;
 
 //Connect DB
 db.connect();
+
+app.use(cors());
 
 app.use(express.urlencoded({
     extended: true
